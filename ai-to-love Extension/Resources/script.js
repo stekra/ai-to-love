a4fdabeedaf6a97e63be53ab8555964089b30835
stekra <stekra@me.com>
2023-02-03 19:24:13 +0100

    Fix grammar in regex

--- ai-to-love Extension/Resources/script.js
+++ ai-to-love Extension/Resources/script.js
@@ -7,7 +7,7 @@ function replaceInText(element) {
         if (node.nodeType == Node.ELEMENT_NODE) {
             replaceInText(node);
         } else if (node.nodeType == Node.TEXT_NODE) {
-            node.textContent = node.textContent.replace(/\b[Aa]n [Aa][Ii]\b/g, 'a love');
+            node.textContent = node.textContent.replace(/\b[Aa]n [Aa][Ii]\b/g, 'love');
             node.textContent = node.textContent.replace(/\b[Aa][Ii]\b/g, 'love');
             node.textContent = node.textContent.replace(/\bOpenAI\b/g, 'OpenLove');
         }
