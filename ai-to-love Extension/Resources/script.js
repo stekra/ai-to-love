ba624cfaf1ce0fa8980fa66ad4e0e2cf9493cde8
stekra <stekra@me.com>
2023-02-03 18:20:05 +0100

    Create extension

--- ai-to-love Extension/Resources/script.js
+++ ai-to-love Extension/Resources/script.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
+    replaceInText(document);
 });
+
+function replaceInText(element) {
+    for (let node of element.childNodes) {
+        if (node.nodeType == Node.ELEMENT_NODE) {
+            replaceInText(node);
+        } else if (node.nodeType == Node.TEXT_NODE) {
+            node.textContent = node.textContent.replace(/\b[Aa]n [Aa][Ii]\b/g, 'a love');
+            node.textContent = node.textContent.replace(/\b[Aa][Ii]\b/g, 'love');
+            node.textContent = node.textContent.replace(/\bOpenAI\b/g, 'OpenLove');
+        }
+    }
+}
